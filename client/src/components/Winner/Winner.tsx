384cb4a9664d621ef3a5e397fefcd1950b78b754
amamenko <amamenko@hotmail.com>
2022-05-29 02:04:17 -0400

    Added additional line break before path share in winner component share link

--- client/src/components/Winner/Winner.tsx
+++ client/src/components/Winner/Winner.tsx
@@ -228,7 +228,7 @@ export const Winner = React.forwardRef<FullRewardElement, any>((props, ref) => {
   }`;
 
   const finalShareText = `${shareText}\n${finalEmojiGrid}\n${
-    pathRankCount.rank && pathRankCount.count ? `${pathShare}\n` : ""
+    pathRankCount.rank && pathRankCount.count ? `\n${pathShare}\n` : ""
   }hollywoodle.ml`;
 
   return (
