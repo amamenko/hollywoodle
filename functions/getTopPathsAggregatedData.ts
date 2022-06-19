f5303bcf73003c6067f111223ed61a8b74373644
amamenko <amamenko@hotmail.com>
2022-06-19 01:08:29 -0400

    Added existence check to get top paths function

--- functions/getTopPathsAggregatedData.ts
+++ functions/getTopPathsAggregatedData.ts
@@ -3,18 +3,27 @@ export const getTopPathsAggregatedData = (
     [key: string]: string | number;
   }[]
 ) => {
-  const allCounts = paths.map((el) => Number(el.count));
-  const allDegrees = paths.map((el) => Number(el.degrees));
+  if (paths) {
+    const allCounts = paths.map((el) => Number(el.count));
+    const allDegrees = paths.map((el) => Number(el.degrees));
 
-  const totalPathsFound = paths.length;
-  const totalPlayers = allCounts.reduce((a, b) => a + b, 0);
-  const lowestDegree = Math.min(...allDegrees);
-  const highestDegree = Math.max(...allDegrees);
+    const totalPathsFound = paths.length;
+    const totalPlayers = allCounts.reduce((a, b) => a + b, 0);
+    const lowestDegree = Math.min(...allDegrees);
+    const highestDegree = Math.max(...allDegrees);
 
-  return {
-    totalPathsFound,
-    totalPlayers,
-    lowestDegree,
-    highestDegree,
-  };
+    return {
+      totalPathsFound,
+      totalPlayers,
+      lowestDegree,
+      highestDegree,
+    };
+  } else {
+    return {
+      totalPathsFound: 0,
+      totalPlayers: 0,
+      lowestDegree: 0,
+      highestDegree: 0,
+    };
+  }
 };
