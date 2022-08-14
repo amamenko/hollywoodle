e03c8bb5b695f5f42eba761d8920785b160daa3e
amamenko <amamenko@hotmail.com>
2022-08-14 18:20:14 -0400

    Added react-snap for dynamic SEO

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { hydrate, render } from "react-dom";
 import App from "./App";
 import "./index.scss";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const rootElement = document.getElementById("root");
+if (rootElement?.hasChildNodes()) {
+  hydrate(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    rootElement
+  );
+} else {
+  render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    rootElement
+  );
+}
