30d449bc4241c8196c1349009b1eecf845989abb
amamenko <amamenko@hotmail.com>
2022-08-30 23:40:35 -0400

    Added IP address parsing to event logger in case of multiple IP string

--- functions/handleEventLog.ts
+++ functions/handleEventLog.ts
@@ -1,7 +1,8 @@
 import { Stats } from "request-stats";
 
 export const handleEventLog = (stats: Stats) => {
+  const ip = stats.req.ip ? stats.req.ip.split(",")[0] : "";
   console.log(
-    `method=${stats.req.method} path="${stats.req.path}" ip="${stats.req.ip}" status=${stats.res.status}`
+    `method=${stats.req.method} path="${stats.req.path}" ip="${ip}" status=${stats.res.status}`
   );
 };
