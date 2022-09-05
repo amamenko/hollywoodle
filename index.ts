42d3f2f60f35649c4720e77f25e33998836c208f
amamenko <amamenko@hotmail.com>
2022-09-05 14:44:09 -0400

    Added perMessageDeflate false flag to websocket server

--- index.ts
+++ index.ts
@@ -54,7 +54,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(enforce.HTTPS({ trustProtoHeader: true }));
 }
 
-const wss = new WebSocketServer({ server });
+const wss = new WebSocketServer({ server, perMessageDeflate: false });
 
 wss.on("connection", (socket: any, req: Request) => {
   function heartbeat() {
